ba562deeb69464e9fd34adf35ca729f70039c432
diegorvj <riveradiegoa@gmail.com>
2023-11-15 16:54:33 -0300

    Catalogo sin sesion

--- index.php
+++ index.php
@@ -6,17 +6,12 @@ require_once 'conexion.php';
 
 if (isset($_SESSION["usuario"])) {
     $correo = $_SESSION["usuario"];
-} else {
-    // El correo no está definido en la sesión.
-    // Puede que haya un problema en la autenticación o redirección.
-    header("Location: login.php");
-    exit();
 }
 
 // Cerrar sesión
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["cerrar_sesion"])) {
     session_destroy();
-    header("Location: login.php"); // Redirigir al inicio de sesión después de cerrar la sesión
+    header("Location: index.php"); // Redirigir al inicio de sesión después de cerrar la sesión
     exit();
 }
 ?>
@@ -35,10 +30,31 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["cerrar_sesion"])) {
     <nav class="navbar navbar-light bg-light shadow-sm">
         <div class="container">
             <span style="font-size: 20px;">
-                <i class="fa-solid fa-user"></i> <?php echo $correo; ?>
+                <i class="fa-solid fa-user"></i> 
+                <?php 
+                    
+                    // Mostrar $correo solo si está definido
+                    if (isset($correo)) {
+                        echo $correo;
+                ?>
+                        
+                        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+                            <button type="submit" class="btn btn-danger" name="cerrar_sesion">Cerrar Sesión</button>
+                        </form>
+
+                <?php 
+
+                    } else {
+                        // Mostrar el botón "Iniciar Sesión" si el usuario no está autenticado
+                        echo '<a href="login.php" class="btn btn-outline-dark">Iniciar Sesión</a>';
+                        echo '<a href="register.php" class="btn btn-outline-dark">Registrarse</a>';
+                        
+                    }
+                
+                ?>
             </span>
             <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-                <button type="submit" class="btn btn-danger" name="cerrar_sesion">Cerrar Sesión</button>
+                
             </form>
             <span style="cursor: pointer;font-size: 20px; text-align: right;">
                 <a href="carro.php" style="text-decoration: none; color: black;">
@@ -47,7 +63,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["cerrar_sesion"])) {
             </span>
         </div> 
     </nav>
-    
+
     <div class="container shadow-sm rounded p-2 mt-2">
         <h2>Búsqueda de documentos:</h2>
         <form action="buscar_documentos.php" method="get" class="p-3" id="filtroForm">
@@ -55,6 +71,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["cerrar_sesion"])) {
                 <label for="document_type" class="col-sm-3 col-form-label">Tipo de documento: </label>
                 <div class="col-sm-9">
                     <select id="document_type" name="document_type" class="form-select">
+                        <option value="" disabled selected>Elegir tipo</option>
                         <option>Revista</option>
                         <option>Articulo</option>
                         <option>Libro</option>
@@ -65,6 +82,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["cerrar_sesion"])) {
                 <label for="category" class="col-sm-3 col-form-label">Categoría: </label>
                 <div class="col-sm-9">
                     <select id="category" name="category" class="form-select">
+                        <option value="" disabled selected>Elegir categoría</option>
                         <option>Tecnologia</option>
                         <option>Ciencia</option>
                         <option>Arte</option>
@@ -90,10 +108,59 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["cerrar_sesion"])) {
                 </div>
             </div>
             <div style="text-align: right;">
-                <button class="btn btn-outline-dark">Volver</button>
                 <button class="btn btn-dark" id="aplicarFiltroBtn">Aplicar filtro</button>
             </div>
         </form>
+    </div>
+
+    <?php
+    // Realizar la consulta a la base de datos
+    $sql = "SELECT * FROM Documento";
+    $resultado = oci_parse($conn, $sql);
+    oci_execute($resultado);
+    ?>
+
+    <div class="container shadow-sm rounded p-2 mt-2">
+        <h2>Todos los documentos: </h2>
+        <div class="p-1 mb-3" style="overflow: scroll; max-height:300px;">
+            <table class="table table-striped">
+
+                <thead>
+                    <tr>
+                        <th scope="col">Título</th>
+                        <th scope="col">Autor</th>
+                        <th scope="col">Edición</th>
+                        <th scope="col">Año</th>
+                        <th scope="col">Tipo</th>
+                        <th scope="col">Categoría</th>
+                        <th scope="col">#</th>
+                        <th scope="col">Agregar</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <?php
+                    // Iterar sobre los resultados y mostrar cada fila
+                    while ($fila = oci_fetch_assoc($resultado)) {
+                        echo "<tr>";
+                        echo "<td>{$fila['TITULO']}</td>";
+                        echo "<td>{$fila['AUTOR']}</td>";
+                        echo "<td>{$fila['EDICION']}</td>";
+                        echo "<td>{$fila['ANIO']}</td>";
+                        echo "<td>{$fila['TIPO']}</td>";
+                        echo "<td>{$fila['CATEGORIA']}</td>";
+                        echo "<td>3</td>";
+                        echo "<td><input type='checkbox'></td>";
+                        echo "</tr>";
+                    }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+        <div style="text-align: right;">
+            <a href="index.php" class="btn btn-outline-dark">Volver</a>
+            <button class="btn btn-dark">Agregar a Solicitud</button>
+        </div>
     </div>
 
     <?php
