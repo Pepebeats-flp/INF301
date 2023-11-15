e695074599bc9ca503288ad647ca26570e7acddf
benjaminzunino <56267038+benjaminzunino@users.noreply.github.com>
2023-11-15 19:52:41 -0300

    Carrito a medio hacer

--- carro.php
+++ carro.php
@@ -19,7 +19,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["cerrar_sesion"])) {
     header("Location: index.php"); // Redirigir al inicio de sesión después de cerrar la sesión
     exit();
 }
+
+// Verificar si la variable de sesión "carrito" está definida y no es null
+if (isset($_SESSION["carrito"]) && is_array($_SESSION["carrito"])) {
+    $carrito = $_SESSION["carrito"];
+} else {
+    // Si el carrito no está definido o es null, muestra un mensaje indicando que está vacío
+    echo "El carrito está vacío.";
+}
 ?>
+
 
 
 <!DOCTYPE html>
@@ -27,22 +36,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["cerrar_sesion"])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Consulta de catalogo</title>
+    <title>Consulta de catálogo</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="catalogo.css">
     <script src="https://kit.fontawesome.com/a4490af95b.js" crossorigin="anonymous"></script>
 </head>
 <body>
-    <nav class="navbar navbar-light bg-light shadow-sm">
-        <div class="container">
-            <span style="font-size: 20px;">
-                <i class="fa-solid fa-user"></i> Carlos Lagos
-            </span>
-            <span style="cursor: pointer;font-size: 20px; text-align: right;">
-                <i class="fa-solid fa-cart-shopping"></i>
-            </span>
-        </div> 
-    </nav>
+    <!-- ... (código del encabezado) ... -->
 
     <div class="container shadow-sm rounded p-2 mt-5">
         <h2>Carro: </h2>
@@ -61,42 +61,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["cerrar_sesion"])) {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>Ingeniería de Software</td>
-                        <td>Roger S. Pressman</td>
-                        <td>4° Edición</td>
-                        <td>2000</td>
-                        <td>Libro Técnico</td>
-                        <td>Ingeniería Informatica</td>
-                        <td>3</td>
-                        <td>
-                            <input type="checkbox">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Ingeniería de Software</td>
-                        <td>Roger S. Pressman</td>
-                        <td>4° Edición</td>
-                        <td>2000</td>
-                        <td>Libro Técnico</td>
-                        <td>Ingeniería Informatica</td>
-                        <td>3</td>
-                        <td>
-                            <input type="checkbox">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Ingeniería de Software</td>
-                        <td>Roger S. Pressman</td>
-                        <td>4° Edición</td>
-                        <td>2000</td>
-                        <td>Libro Técnico</td>
-                        <td>Ingeniería Informatica</td>
-                        <td>3</td>
-                        <td>
-                            <input type="checkbox">
-                        </td>
-                    </tr>
+                    <?php
+                    // Recorrer los elementos del carrito almacenados en la sesión
+                    foreach ($_SESSION['carrito'] as $item) {
+                        echo "<tr>";
+                        echo "<td>{$item['TITULO']}</td>";
+                        echo "<td>{$item['AUTOR']}</td>";
+                        echo "<td>{$item['EDICION']}</td>";
+                        echo "<td>{$item['ANIO']}</td>";
+                        echo "<td>{$item['TIPO']}</td>";
+                        echo "<td>{$item['CATEGORIA']}</td>";
+                        echo "<td>{$item['CANTIDAD']}</td>";
+                        echo "<td><input type='checkbox'></td>";
+                        echo "</tr>";
+                    }
+                    ?>
                 </tbody>
             </table>
         </div>
