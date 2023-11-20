74ff8534c11b477c8304ce396ff61adaf8ec8fe7
diegorvj <riveradiegoa@gmail.com>
2023-11-20 17:55:49 -0300

    Creacion documentos

---
+++ agregar_documento.php
@@ -0,0 +1,137 @@
+<?php
+session_start();
+
+// Incluir la conexión a la base de datos
+require_once 'conexion.php';
+
+if (isset($_SESSION["usuario"])) {
+    $usuario = $_SESSION["usuario"];
+}
+
+// Cerrar sesión
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["cerrar_sesion"])) {
+    session_destroy();
+    header("Location: index.php"); // Redirigir al inicio de sesión después de cerrar la sesión
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Consulta de catálogo</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="catalogo.css">
+    <script src="https://kit.fontawesome.com/a4490af95b.js" crossorigin="anonymous"></script>
+</head>
+
+<!-- Body Responsivo para usuarios y bibliotecarios -->
+<body>
+    <nav class="navbar navbar-light bg-light shadow-sm">
+        <div class="container">
+
+            <span style="font-size: 20px;">
+                <i class="fa-solid fa-user"></i> 
+
+                <?php 
+                    if (isset($usuario)) {
+                        echo "Bibliotecario(a): ", $usuario;
+                ?>
+                        
+                        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+                            <button type="submit" class="btn btn-danger" name="cerrar_sesion">Cerrar Sesión</button>
+                        </form>
+
+                <?php 
+                    } else {
+                        echo '<a href="login.php" class="btn btn-outline-dark">Iniciar Sesión</a>';
+                        echo '<a href="register.php" class="btn btn-outline-dark">Registrarse</a>';
+                    }
+                ?>
+            </span>
+
+            <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+                <!-- Otro formulario u otros elementos según sea necesario -->
+            </form>
+
+        </div> 
+    </nav>
+
+    <div class="container mt-5">
+        <h2>Agregar Documento</h2>
+        
+        
+        <form action="procesar_creacion_doc.php" method="post">
+            <div class="form-group mt-2">
+                <label for="titulo">Título:</label>
+                <input type="text" class="form-control" name="titulo" required>
+            </div>
+
+            <br>
+
+            <div class="form-group mt-2">
+                <label for="autor">Autor:</label>
+                <input type="text" class="form-control" name="autor" required>
+            </div>
+
+            <br>
+
+            <div class="form-group">
+                <label for="edicion">Edición:</label>
+                <input type="text" class="form-control" name="edicion">
+            </div>
+
+            <br>
+
+            <div class="form-group mt-2">
+                <label for="editorial">Editorial:</label>
+                <input type="text" class="form-control" name="editorial">
+            </div>
+
+            <br>
+
+            <div class="form-group mt-2">
+                <label for="anio">Año:</label>
+                <input type="number" class="form-control" name="anio">
+            </div>
+
+            <br>
+
+            <div class="form-group mt-2">
+                <label for="tipo">Tipo:</label>
+                <select class="form-control" name="tipo">
+                    <option value="Libro">Libro</option>
+                    <option value="Revista">Revista</option>
+                    <option value="Articulo">Articulo</option>
+                </select>
+            </div>
+
+            <br>
+
+            <div class="form-group mt-2">
+                <label for="categoria">Categoría:</label>
+                <select class="form-control" name="categoria">
+                    <option value="Ciencia">Ciencia</option>
+                    <option value="Tecnologia">Tecnología</option>
+                    <option value="Arte">Arte</option>
+                </select>
+            </div>
+
+            <br>
+
+            <div class="form-group mt-2">
+                <label for="cantidad">Cantidad:</label>
+                <input type="number" class="form-control" name="cantidad">
+            </div>
+
+            <br>
+
+            <button type="submit" class="btn btn-dark">Agregar Documento</button>
+        </form>
+    </div>
+</body>
+
+</html>
---
+++ procesar_creacion_doc.php
@@ -0,0 +1,69 @@
+<?php
+session_start();
+
+// Incluir la conexión a la base de datos (asumiendo que ya tienes esto configurado en 'conexion.php')
+require_once 'conexion.php';
+
+if (isset($_SESSION["usuario"])) {
+    $usuario = $_SESSION["usuario"];
+}
+
+// Cerrar sesión
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["cerrar_sesion"])) {
+    session_destroy();
+    header("Location: index.php"); // Redirigir al inicio de sesión después de cerrar la sesión
+    exit();
+}
+
+// Procesar el formulario de agregar documento
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["titulo"], $_POST["autor"], $_POST["edicion"], $_POST["editorial"], $_POST["anio"], $_POST["tipo"], $_POST["categoria"], $_POST["cantidad"])) {
+
+    // Obtener los datos del formulario
+    $titulo = $_POST["titulo"];
+    $autor = $_POST["autor"];
+    $edicion = $_POST["edicion"];
+    $editorial = $_POST["editorial"];
+    $anio = $_POST["anio"];
+    $tipo = $_POST["tipo"];
+    $categoria = $_POST["categoria"];
+    $cantidad = $_POST["cantidad"];
+
+    // Preparar la consulta SQL con marcadores de posición
+    $sql = "INSERT INTO Documento (Tipo, Titulo, Autor, Editorial, Anio, Edicion, Categoria, Cantidad) 
+            VALUES (:tipo, :titulo, :autor, :editorial, :anio, :edicion, :categoria, :cantidad)";
+
+    // Preparar la declaración OCI8
+    $stmt = oci_parse($conn, $sql);
+
+    // Vincular parámetros
+    oci_bind_by_name($stmt, ":tipo", $tipo);
+    oci_bind_by_name($stmt, ":titulo", $titulo);
+    oci_bind_by_name($stmt, ":autor", $autor);
+    oci_bind_by_name($stmt, ":editorial", $editorial);
+    oci_bind_by_name($stmt, ":anio", $anio);
+    oci_bind_by_name($stmt, ":edicion", $edicion);
+    oci_bind_by_name($stmt, ":categoria", $categoria);
+    oci_bind_by_name($stmt, ":cantidad", $cantidad);
+
+    // Ejecutar la consulta
+    $result = oci_execute($stmt);
+
+    if ($result) {
+        // Éxito al insertar el documento
+        echo '<div class="alert alert-danger" role="alert">
+        A simple danger alert—check it out!
+      </div>';
+        header("Location: indexbiblio.php?documento_creado=1");
+        exit();
+    } else {
+        // Error al insertar el documento
+        $error = oci_error($stmt);
+        echo "Error al agregar el documento: " . $error["message"];
+    }
+
+    // Liberar recursos
+    oci_free_statement($stmt);
+    oci_close($conn);
+}
+?>
+
